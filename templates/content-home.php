9c85edaae4c540952703925406d1cbacb81c03e2
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2015-03-18 14:20:49 -0700

    Front page layout, images

--- templates/content-home.php
+++ templates/content-home.php
@@ -5,11 +5,82 @@
   </div>
 </div>
 <div class="row">
-  <div class="col-sm-2">
-    <p class="oldnews-lg">$10adv</p>
+  <div class="col-xs-6">
+    <p class="oldnews-static starbg">$10adv</p>
   </div>
-  <div class="col-sm-2 col-sm-offset-8 text-center">
-    $16dos
+  <div class="col-xs-6">
+    <p class="oldnews-static starbg pull-right">$16dos</p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12 scroll-top">
+    <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/scroll-top.png" alt="Scroll Top" class="img-responsive" aria-hidden="true">
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <p class="oldnews-md text-center">A celebration of Americana, Roots &amp; Bluegrass music;<br/> &amp; craft beverages made in the city of Yakima</p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/scroll-bottom.png" alt="Scroll Bottom" class="img-responsive" aria-hidden="true">
+  </div>
+</div>
+<div class="bands">
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-xxxl text-center text-uppercase"><a href="bands">SLEEPY MAN BANJO BOYS</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-xxxl text-center"><a href="bands">POLECAT</a> <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/star.png" alt="star" aria-hidden="true"> <a href="bands">FRUITION</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-lg text-center text-uppercase"><a href="bands">HILLSTOMP</a> <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/star.png" alt="star" aria-hidden="true"> <a href="bands">Blake Noble Band</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-md text-center text-uppercase"><a href="bands">Lil' Smokies</a> <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/star.png" alt="star" aria-hidden="true"> <a href="bands">VAUDEVILLE ETIQUETE</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-md text-center text-uppercase"><a href="bands">THE COMMON DEER</a> <img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/star.png" alt="star" aria-hidden="true"> <a href="\bands">JOHN DUNNIGAN</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-xxxl text-center text-uppercase">SATURDAY, MAY 16TH</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-md text-center text-uppercase">HISTORIC NORTH FRONT STREET</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <p class="oldnews-xs text-center text-uppercase">TICKETS AT CAPITOL THEATRE BOX OFFICE OR CALL 853-ARTS</p>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-3">
+    <a href="http://www.1041kxdd.com/" target="_blank"><img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/kxdd.png" alt="Scroll Bottom" class="img-responsive" aria-hidden="true"></a>
+  </div>
+  <div class="col-xs-3">
+    <a href="http://www.kimatv.com/" target="_blank"><img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/kima.png" alt="Scroll Bottom" class="img-responsive" aria-hidden="true"></a>
+  </div>
+  <div class="col-xs-3">
+    <a href="http://www.kunwtv.com/" target="_blank"><img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/kunw.png" alt="Scroll Bottom" class="img-responsive" aria-hidden="true"></a>
+  </div>
+  <div class="col-xs-3">
+    <a href="http://www.980thetractor.com/" target="_blank"><img src="/wordpress/wp-content/themes/roots-and-vines-wp-theme/dist/images/980am.png" alt="Scroll Bottom" class="img-responsive" aria-hidden="true"></a>
   </div>
 </div>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
